41eed98bcbec33f3cdc84fb9bd05ba176906bd40
KasunKVI <kkhettiarachchi10@gmail.com>
2024-01-17 12:13:22 +0530

    implement JNDI for the project

--- src/main/java/software/kasunkavinda/pos_system_backend/util/FactoryConfiguration.java
+++ src/main/java/software/kasunkavinda/pos_system_backend/util/FactoryConfiguration.java
@@ -8,33 +8,41 @@ import software.kasunkavinda.pos_system_backend.entity.Item;
 import software.kasunkavinda.pos_system_backend.entity.Orders;
 import software.kasunkavinda.pos_system_backend.entity.User;
 
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+import javax.sql.DataSource;
+
 public class FactoryConfiguration {
 
     private static FactoryConfiguration factoryConfiguration;
     private SessionFactory sessionFactory;
 
-    private FactoryConfiguration(){
+    private FactoryConfiguration() {
+        try {
 
 
-
-        Configuration configuration = new Configuration().configure("hibernate.cfg.xml");
-                configuration.addAnnotatedClass(Customer.class);
-                configuration.addAnnotatedClass(Item.class);
-                configuration.addAnnotatedClass(Orders.class);
-                configuration.addAnnotatedClass(User.class);
-
-        sessionFactory = configuration.buildSessionFactory();
+            DataSource dataSource = (DataSource) new InitialContext().lookup("java:comp/env/jdbc/pos_system");
 
 
+            Configuration configuration = new Configuration().configure("hibernate.cfg.xml");
+            configuration.addAnnotatedClass(Customer.class);
+            configuration.addAnnotatedClass(Item.class);
+            configuration.addAnnotatedClass(Orders.class);
+            configuration.addAnnotatedClass(User.class);
+            configuration.setProperty("hibernate.connection.datasource", "java:comp/env/jdbc/pos_system");// Use JNDI to get the DataSource
+
+            sessionFactory = configuration.buildSessionFactory();
+
+        } catch (NamingException e) {
+            e.printStackTrace(); // Handle the exception appropriately
+        }
     }
 
-    public static FactoryConfiguration getInstance(){
-        return (factoryConfiguration==null)? factoryConfiguration=new FactoryConfiguration(): factoryConfiguration;
-
+    public static FactoryConfiguration getInstance() {
+        return (factoryConfiguration == null) ? factoryConfiguration = new FactoryConfiguration() : factoryConfiguration;
     }
 
-    public Session getSession(){
-
-        return  sessionFactory.openSession();
+    public Session getSession() {
+        return sessionFactory.openSession();
     }
 }
