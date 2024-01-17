acece3783647d699d13016434785692b6fdcf8ca
KasunKVI <kkhettiarachchi10@gmail.com>
2024-01-17 08:26:04 +0530

    implement logs for order api

--- src/main/java/software/kasunkavinda/pos_system_backend/api/OrderApi.java
+++ src/main/java/software/kasunkavinda/pos_system_backend/api/OrderApi.java
@@ -10,6 +10,8 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import org.hibernate.Session;
 import org.hibernate.Transaction;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import software.kasunkavinda.pos_system_backend.dto.OrderDateDto;
 import software.kasunkavinda.pos_system_backend.entity.Customer;
 import software.kasunkavinda.pos_system_backend.entity.Orders;
@@ -26,6 +28,8 @@ public class OrderApi extends HttpServlet {
     static Session session;
 
     static Transaction transaction;
+
+    private static final Logger logger = LoggerFactory.getLogger(OrderApi.class);
 
     private static void getConnection() {
 
@@ -46,6 +50,8 @@ public class OrderApi extends HttpServlet {
 
                 List<Orders> orders = session.createNativeQuery("SELECT * FROM Orders", Orders.class).list();
                 List<OrderDateDto> orderDateDtos = new ArrayList<>();
+
+                logger.info("Get all orders details from database");
 
                 for(Orders order: orders){
                     orderDateDto = new OrderDateDto();
@@ -75,6 +81,8 @@ public class OrderApi extends HttpServlet {
                 orders.setCustomer(null);
                 orders.setItems(null);
 
+                logger.info("Get selected order from database");
+
                 OrderDateDto order_detail = new OrderDateDto(orders.getId(), orders.getDate(), orders.getBalance(), customer.getNic(), customer.getName());
                 Gson gson = new Gson(); // Import Gson library for JSON manipulation
                 String jsonCustomer = gson.toJson(order_detail);
@@ -88,7 +96,7 @@ public class OrderApi extends HttpServlet {
             }
 
         } catch (Exception e) {
-            e.printStackTrace();
+            logger.error("Error message",e);
             resp.sendError(HttpServletResponse.SC_INTERNAL_SERVER_ERROR, "Internal server error");
         } finally {
             session.close();
@@ -103,6 +111,7 @@ public class OrderApi extends HttpServlet {
             if (req.getContentType() == null ||
                     !req.getContentType().toLowerCase().startsWith("application/json")) {
                 resp.sendError(HttpServletResponse.SC_BAD_REQUEST);
+                logger.warn("Error on request type");
             } else {
                 Jsonb jsonb = JsonbBuilder.create();
                 var updatedOrder = jsonb.fromJson(req.getReader(), OrderDateDto.class);
@@ -116,6 +125,7 @@ public class OrderApi extends HttpServlet {
                 orders.setDate(updatedOrder.getDate());
                 orders.setBalance(updatedOrder.getBalance());
 
+                logger.info("Update selected order");
                 session.update(orders);
 
 
@@ -124,10 +134,10 @@ public class OrderApi extends HttpServlet {
                 transaction.commit();
             } catch (Exception e) {
                 transaction.rollback();
-                e.printStackTrace();
+                logger.error("Error message",e);
             }
         } catch (Exception e) {
-            e.printStackTrace();
+            logger.error("Error message",e);
             resp.sendError(HttpServletResponse.SC_INTERNAL_SERVER_ERROR, "Internal server error");
         } finally {
             session.close();
@@ -147,12 +157,12 @@ public class OrderApi extends HttpServlet {
             if (orders != null) {
                 session.remove(orders);
                 transaction.commit();
-                session.close();
+                logger.info("Delete selected order");
             }else {
                 resp.sendError(HttpServletResponse.SC_NOT_FOUND, "Customer not found");
             }
         } catch (Exception e) {
-            e.printStackTrace();
+            logger.error("Error message",e);
             resp.sendError(HttpServletResponse.SC_INTERNAL_SERVER_ERROR, "Internal server error");
         } finally {
             session.close();
